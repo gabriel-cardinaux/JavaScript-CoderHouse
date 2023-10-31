705a1c33a270a02dbe672e6cd11e58ffa8de9d9d
gabriel-cardinaux <gabriel.cardinaux@gmail.com>
2023-10-30 21:26:41 -0300

    Commit 1.0.0.1
    Falta terminar el modal del carrito, tocar el html, estilos, etc

--- main.js
+++ main.js
@@ -5,17 +5,19 @@
 // Arrays con lista de productos
 
 const shopContent = document.getElementById("shopContent");
+const checkShopCart = document.getElementById("CheckShopCart");
+const modalWindow = document.getElementById("modalWindow");
 
 const products = [
-    {id:1, name: "English book starter", price: 2000, img;,},
-    {id:2, name: "English book intermediate", price: 2500, img:,},
-    {id:3, name: "English book advanced", price: 3000, img:,},
-    {id:4, name: "French book starter", price: 2500, img:,},
-    {id:5, name: "French book intermediate", price: 3000, img:,},
-    {id:6, name: "French book advanced", price: 3500, img:,},
-    {id:7, name: "Portuguese book starter", price: 1500, img:,},
-    {id:8, name: "Portuguese book intermediate", price: 2500, img:,},
-    {id:9, name: "Portuguese book advanced", price: 3000, img:,},
+    {id:1, name: "English book starter", price: 2000, img:"https://acdn.mitiendanube.com/stores/934/273/products/9780194202374-es1-ba9db924c279c4cf5e15541542013234-480-0.jpg",},
+    {id:2, name: "English book intermediate", price: 2500, img:"https://pictures.abebooks.com/isbn/9780194648851-us.jpg",},
+    {id:3, name: "English book advanced", price: 3000, img:"https://images.cdn1.buscalibre.com/fit-in/360x360/0f/cf/0fcfa824a655009603bde4ad8409485e.jpg",},
+    {id:4, name: "French book starter", price: 2500, img:"https://m.media-amazon.com/images/I/51ESyWeXLAL.jpg",},
+    {id:5, name: "French book intermediate", price: 3000, img:"https://m.media-amazon.com/images/I/61C85PXsDqL._AC_UF1000,1000_QL80_.jpg",},
+    {id:6, name: "French book advanced", price: 3500, img:"https://m.media-amazon.com/images/I/61KoaZt55KL._AC_UF1000,1000_QL80_.jpg",},
+    {id:7, name: "Portuguese book starter", price: 1500, img:"https://m.media-amazon.com/images/I/61OGQfuwoCL._AC_UF1000,1000_QL80_.jpg",},
+    {id:8, name: "Portuguese book intermediate", price: 2500, img:"https://m.media-amazon.com/images/I/71+Twv3C3jL._AC_UF1000,1000_QL80_.jpg",},
+    {id:9, name: "Portuguese book advanced", price: 3000, img:"https://static.wixstatic.com/media/42698f_8f168ab4ba954609965fea8fc947657b~mv2.jpg/v1/fill/w_562,h_768,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Aquarela%20Advanced_COVER_2021.jpg",},
 
 ];
 // Carrito función
@@ -33,8 +35,42 @@ products.forEach((product) => {
     buy.innerText = "Buy";
     content.append(buy)
     buy.className = "buy";
+
+    buy.addEventListener("click", () =>{
+        shoppingCart.push({
+            id: product.id,
+            name: product.name,
+            price: product.price,
+            img: product.img,
+        })
+    })
 });
-let selection = prompt("Do you want to buy any product available? Yes or No?")
+
+checkShopCart.addEventListener("click", () =>{
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header"
+    modalHeader.innerHTML = `
+    <h1 class="modal-tittle">Shop Cart</h1>
+    `;
+    modalWindow.append(modalHeader);
+    const modalButton = document.createElement("h1");
+    modalButton.innerText = "x";
+    modalButton.className = "modal-header-button";
+
+    modalHeader.append(modalButton);
+
+    shoppingCart.forEach((product) => {
+    let shoppingCartContent = document.createElement("div")
+    shoppingCart.className = "modal-window-content"
+    shoppingCart.innerHTML = `
+    <img src="${product.img}">
+    <h3>${product.name}</h3>
+    <p>${product.price} $</p>
+    `;
+    })
+    
+})
+/*let selection = prompt("Do you want to buy any product available? Yes or No?")
 //Bucle inicial
 while(selection != "Yes" && selection != "No"){
     alert("Please select Yes or No")
@@ -96,8 +132,8 @@ while(selection != "No"){
         }
         selection = prompt("Do you want to add any other product")
 //Cierre de bucle     
-        while(selection == "No"){
-            alert("Thanks for your purchase")
+       while(selection == "No"){
+          alert("Thanks for your purchase")
             shoppingCart.forEach((shoppingCartTotal) =>{
                 alert(`product: ${shoppingCartTotal.product}, quantity: ${shoppingCartTotal.quantity}, Total to pay ${shoppingCartTotal.quantity * shoppingCartTotal.price}`)
             })
@@ -106,4 +142,4 @@ while(selection != "No"){
 }
 // Metodo reduce
 const total = shoppingCart.reduce((acc, the) => acc + the.price * the.quantity, 0)
-alert(`Total to pay: ${total}`)
+alert(`Total to pay: ${total}`)*/
