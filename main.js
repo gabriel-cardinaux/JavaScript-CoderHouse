84c66e192da58aaae04372ad0ca8b74da718d6f6
gabriel-cardinaux <gabriel.cardinaux@gmail.com>
2023-11-10 00:28:23 -0300

    minor changes

--- main.js
+++ main.js
@@ -7,21 +7,22 @@
 const shopContent = document.getElementById("shopContent");
 const checkShopCart = document.getElementById("CheckShopCart");
 const modalWindow = document.getElementById("modalWindow");
+const shopCartQuantity = document.getElementById("shopCartQuantity");
 
 const products = [
-    {id:1, name: "English book starter", price: 2000, img:"https://acdn.mitiendanube.com/stores/934/273/products/9780194202374-es1-ba9db924c279c4cf5e15541542013234-480-0.jpg",},
-    {id:2, name: "English book intermediate", price: 2500, img:"https://pictures.abebooks.com/isbn/9780194648851-us.jpg",},
-    {id:3, name: "English book advanced", price: 3000, img:"https://images.cdn1.buscalibre.com/fit-in/360x360/0f/cf/0fcfa824a655009603bde4ad8409485e.jpg",},
-    {id:4, name: "French book starter", price: 2500, img:"https://m.media-amazon.com/images/I/51ESyWeXLAL.jpg",},
-    {id:5, name: "French book intermediate", price: 3000, img:"https://m.media-amazon.com/images/I/61C85PXsDqL._AC_UF1000,1000_QL80_.jpg",},
-    {id:6, name: "French book advanced", price: 3500, img:"https://m.media-amazon.com/images/I/61KoaZt55KL._AC_UF1000,1000_QL80_.jpg",},
-    {id:7, name: "Portuguese book starter", price: 1500, img:"https://m.media-amazon.com/images/I/61OGQfuwoCL._AC_UF1000,1000_QL80_.jpg",},
-    {id:8, name: "Portuguese book intermediate", price: 2500, img:"https://m.media-amazon.com/images/I/71+Twv3C3jL._AC_UF1000,1000_QL80_.jpg",},
-    {id:9, name: "Portuguese book advanced", price: 3000, img:"https://static.wixstatic.com/media/42698f_8f168ab4ba954609965fea8fc947657b~mv2.jpg/v1/fill/w_562,h_768,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Aquarela%20Advanced_COVER_2021.jpg",},
+    {id:1, name: "English book starter", price: 2000, img:"https://acdn.mitiendanube.com/stores/934/273/products/9780194202374-es1-ba9db924c279c4cf5e15541542013234-480-0.jpg", quantity: 1,},
+    {id:2, name: "English book intermediate", price: 2500, img:"https://pictures.abebooks.com/isbn/9780194648851-us.jpg", quantity: 1,},
+    {id:3, name: "English book advanced", price: 3000, img:"https://images.cdn1.buscalibre.com/fit-in/360x360/0f/cf/0fcfa824a655009603bde4ad8409485e.jpg", quantity: 1,},
+    {id:4, name: "French book starter", price: 2500, img:"https://m.media-amazon.com/images/I/51ESyWeXLAL.jpg", quantity: 1,},
+    {id:5, name: "French book intermediate", price: 3000, img:"https://m.media-amazon.com/images/I/61C85PXsDqL._AC_UF1000,1000_QL80_.jpg", quantity: 1,},
+    {id:6, name: "French book advanced", price: 3500, img:"https://m.media-amazon.com/images/I/61KoaZt55KL._AC_UF1000,1000_QL80_.jpg", quantity: 1,},
+    {id:7, name: "Portuguese book starter", price: 1500, img:"https://m.media-amazon.com/images/I/61OGQfuwoCL._AC_UF1000,1000_QL80_.jpg", quantity: 1,},
+    {id:8, name: "Portuguese book intermediate", price: 2500, img:"https://m.media-amazon.com/images/I/71+Twv3C3jL._AC_UF1000,1000_QL80_.jpg", quantity: 1,},
+    {id:9, name: "Portuguese book advanced", price: 3000, img:"https://static.wixstatic.com/media/42698f_8f168ab4ba954609965fea8fc947657b~mv2.jpg/v1/fill/w_562,h_768,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Aquarela%20Advanced_COVER_2021.jpg", quantity: 1,},
 
 ];
 // Carrito función
-let shoppingCart = []
+let shoppingCart = JSON.parse(localStorage.getItem("SaveItems")) || []
 
 products.forEach((product) => {
     let content = document.createElement("div");
@@ -37,15 +38,26 @@ products.forEach((product) => {
     buy.className = "buy";
 
     buy.addEventListener("click", () =>{
+    const repeat = shoppingCart.some((repeatedProduct) => repeatedProduct.id === product.id);
+    if (repeat){
+        shoppingCart.map((prod) =>{
+            if(prod.id === product.id){
+                prod.quantity++;
+            }
+        })
+    } else{
         shoppingCart.push({
             id: product.id,
             name: product.name,
             price: product.price,
             img: product.img,
+            quantity: product.quantity,
         })
+    }
+    shopCartCounter();
+    localSave();
     })
 });
-
 checkShopCart.addEventListener("click", () =>{
     modalWindow.innerHTML = "";
     modalWindow.style.display = "flex";
@@ -71,17 +83,54 @@ checkShopCart.addEventListener("click", () =>{
     <img src="${product.img}">
     <h3>${product.name}</h3>
     <p>${product.price} $</p>
+    <p>Quantity: ${product.quantity}</p>
+    <p>Total: ${product.quantity * product.price}</p>
     `;
     modalWindow.append(shoppingCartContent);
+    
+
+    let eliminate = document.createElement("span");
+    eliminate.innerText = "❎";
+    eliminate.className = "delete-product";
+    shoppingCartContent.append(eliminate);
+    eliminate.addEventListener("click", removeContent)
     });
 
-    const total = shoppingCart.reduce((acc, the) => acc + the.price, 0)
+    const total = shoppingCart.reduce((acc, the) => acc + the.price * the.quantity, 0)
     const totalToPay = document.createElement("div");
     totalToPay.className = "total-content";
     totalToPay.innerHTML = `total to pay: ${total} $`;
     modalWindow.append(totalToPay)
     
 });
+
+const removeContent = () => {
+    const findID = shoppingCart.find((element) => element.id);
+
+    shoppingCart = shoppingCart.filter((shoppingCartID) => {
+        return shoppingCartID !== findID;
+    })
+    shopCartCounter();
+    localSave();
+}
+
+const shopCartCounter = () => {
+    shopCartQuantity.style.display = "block";
+    const shopCartLength = shoppingCart.length;
+    localStorage.setItem("shopCartLength", JSON.stringify(shopCartLength))
+    shopCartQuantity.innerText = JSON.parse(localStorage.getItem("shopCartLength"))
+}
+shopCartCounter ();
+//Local Storage
+
+const localSave = () => {
+localStorage.setItem("SavedItems", JSON.stringify(shoppingCart))
+}
+
+
+
+
+
 /*let selection = prompt("Do you want to buy any product available? Yes or No?")
 //Bucle inicial
 while(selection != "Yes" && selection != "No"){
